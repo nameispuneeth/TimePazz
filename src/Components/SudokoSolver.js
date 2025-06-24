79db0ae69d7bdef686866b7176eb1bbe38db8cf0
nameispuneeth <puneethkumarkaramala@gmail.com>
2025-06-24 17:16:59 +0530

    Sudoko Grid

--- src/Components/SudokoSolver.js
+++ src/Components/SudokoSolver.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import './TicTacToe.css';
+import { useState } from "react";
+import './Sudoko.css'
 
-export default function SudokuGrid() {
-  return (
-    <div className="sudoku-container">
-      <table className="sudoku-grid">
-        <tbody>
-          {Array.from({ length: 9 }, (_, row) => (
-            <tr key={row}>
-              {Array.from({ length: 9 }, (_, col) => (
-                <td
-                  key={col}
-                  className={`cell ${
-                    (Math.floor(row / 3) + Math.floor(col / 3)) % 2 === 0
-                      ? 'cell-block'
-                      : ''
-                  }`}
-                >
-                  <input
-                    type="text"
-                    maxLength="1"
-                    pattern="[1-9]"
-                    className="cell-input"
-                  />
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+export default function Sudoko(){
+  const [sudokuGrid, setSudokuGrid] = useState(
+    Array.from({ length: 9 }, () => Array(9).fill(""))
+  );
+  let handleChange=(row,col,value)=>{
+    let newGrid=sudokuGrid.map((r,rInd)=>(
+      r.map((cell,cInd)=>(rInd==row && cInd==col ? value:cell))
+    ))
+    setSudokuGrid(newGrid);
+  }
+  return(
+    <div className="mt-3">
+     {sudokuGrid.map((row,rInd)=>(
+      <div className="row" key={`${rInd}`}>
+        {row.map((cell,cInd)=>(
+          <input key={`${rInd}-${cInd}`} type="number"  
+          className={`sm
+            ${cInd % 3 === 2 ? 'right-border' : ''}
+            ${rInd % 3 === 2 ? 'bottom-border' : ''}
+            ${cInd === 0 ? 'first-col' : ''}
+            ${rInd === 0 ? 'first-row' : ''}
+          `}
+          min="1" max="9" pattern="[1-9]" value={cell} onChange={(e)=>{
+            let val=e.target.value;
+            if(val==="" || /^[1-9]$/.test(val)
+) handleChange(rInd,cInd,val);
+          }}/>
+        ))}
+      </div>
+     ))}
+     <button className="btn solve mt-3">SOLVE</button>
     </div>
   );
 }
